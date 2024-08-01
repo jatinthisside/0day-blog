4e29c15cf6ea6c12ed819bccb523ef7308a55e67
jatinthisside <jatinkyadu@gmail.com>
2024-08-01 23:49:08 +0900

    Add dburl log for vercel

--- config/db.ts
+++ config/db.ts
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
 
 export const dbConnect=async()=>{
+    console.log("Vercel Degub ->",process.env.DBURL);
+    console.log("Vercel Degub Type ->", typeof(process.env.DBURL));
     mongoose.connect(String(process.env.DBURL)).then(()=>{
+        console.log('Connection String -> ',typeof(process.env.DBURL));
+        console.log('Connection String -> ',typeof(String(process.env.DBURL)));
         console.log("DB Connected Succesfully");
     }).catch((e:any)=>{
         console.log("Error While Connecting to Db -> ",e);
