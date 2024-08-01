eae7d0943c8050bf96b8eb831c699e02f441a429
jatinthisside <jatinkyadu@gmail.com>
2024-08-02 00:15:49 +0900

    update gitignore env

--- config/db.ts
+++ config/db.ts
@@ -5,7 +5,7 @@ export const dbConnect=async()=>{
     console.log("Vercel Degub Type ->", typeof(process.env.DBURL));
     mongoose.connect(String(process.env.DBURL)).then(()=>{
         console.log('Connection String -> ',typeof(process.env.DBURL));
-        console.log('Connection String -> ',typeof(String(process.env.DBURL)));
+        console.log('Connection String -> ',typeof(String(process.env.DBURL)))
         console.log("DB Connected Succesfully");
     }).catch((e:any)=>{
         console.log("Error While Connecting to Db -> ",e);
